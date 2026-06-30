84ebf6f14bdc2fe80edf78db2497f384413ddc57
Daichi Kobayashi <715-student004@la-study.com>
2026-06-30 14:03:49 +0900

    models

---
+++ app/Models/Attendance.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'work_date',
+        'clock_in',
+        'clock_out',
+    ];
+
+    protected $casts = [
+        'work_date' => 'date',
+        'clock_in' => 'datetime',
+        'clock_out' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
